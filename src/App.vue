8dc7cd69d7e2826f8238a8c4e4b07d34f8112d69
kamst01 <kamste01@gmail.com>
2021-09-04 10:34:35 -0400

    add buttons, no functionality.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <script setup>
 import Quote from "./components/Quote.vue";
 import UserTypedText from "./components/UserTypedText.vue";
+import Button from './components/Button.vue';
+</script>
+
+<script>
+export default {
+    data() {
+        return {
+            nextRaceText: 'Next Race',
+            startRaceText: 'Start Race',
+        }
+    },
+}
 </script>
 
 <template>
 <Quote/>
 <UserTypedText/>
+<div class="flex flex-col lg:flex-row items-center justify-start space-x-4 mt-4">
+    <Button @clickTarget="startRace()" buttonText="start race" />
+    <Button @clickTarget="nextRace()" buttonText="next race" />
+</div>
 </template>
---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <button
+    class="px-3 py-2 bg-blue-600 text-white text-base font-semibold rounded"
+    @click="$emit('clickTarget')"
+  >
+      {{ buttonText }}
+  </button>
+</template>
+
+<script>
+export default {
+    props: {
+        buttonText: String
+    },
+    emits: ['clickTarget']
+}
+</script>
