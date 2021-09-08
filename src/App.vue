5c12f676dfd167793a4b39425824b357103d4dcc
kamst01 <kamste01@gmail.com>
2021-09-08 13:31:10 -0400

    move footer into it's own component.

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import Quote from "./components/Quote.vue";
 import UserTypedText from "./components/UserTypedText.vue";
 import Button from './components/Button.vue';
 import Header from "./components/header/Index.vue";
+import Footer from './components/Footer.vue';
 </script>
 
 <script>
@@ -133,13 +134,6 @@ export default {
             </div>
         </main>
         <!-- Footer -->
-        <footer>
-            <div class="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
-                <div class="border-t border-gray-200 py-8 text-sm text-gray-500 text-center sm:text-left">
-                    <span class="block sm:inline">Typizer. </span> 
-                    <span class="block sm:inline">MIT License.</span>
-                </div>
-            </div>
-        </footer>
+        <Footer />
     </div>
 </template>
