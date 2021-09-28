0dc40d6d9fde35d8af72c9f618d9516dc3e80442
Keifer Amster <69758615+kamst01@users.noreply.github.com>
2021-09-28 13:25:25 -0400

    remove unused blue text color.

--- src/components/Typizer.vue
+++ src/components/Typizer.vue
@@ -95,18 +95,18 @@ import Button from './Button.vue';
 </script>
 
 <template>
-    <div class="max-w-3xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+    <div class="max-w-3xl px-4 mx-auto sm:px-6 lg:max-w-7xl lg:px-8">
         <!-- Main 3 column grid -->
-        <div class="grid grid-cols-1 gap-4 items-start lg:grid-cols-3 lg:gap-8">
+        <div class="grid items-start grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8">
             <!-- Left 2 column -->
             <div class="grid grid-cols-1 gap-4 lg:col-span-2">
                 <section aria-labelledby="typizerRaceHeading">
                     <h2 class="sr-only" id="typizerRaceHeading">Typizer Race</h2>
-                    <div class="rounded-lg bg-white overflow-hidden shadow">
+                    <div class="overflow-hidden bg-white rounded-lg shadow">
                         <div class="p-6 space-y-6">
                             <Quote/>
                             <UserTypedText @input="watchRace"/>
-                            <div class="flex flex-row items-center justify-start space-x-4 mt-4">
+                            <div class="flex flex-row items-center justify-start mt-4 space-x-4">
                                 <Button @clickTarget="startRace()">{{ startRaceText }}</Button>
                                 <Button @clickTarget="nextRace()">{{ nextRaceText }}</Button>
                             </div>
@@ -116,7 +116,7 @@ import Button from './Button.vue';
                 <section class="bg-white rounded-lg shadow" aria-labelledby="typizerRaceStatsHeading">
                     <h2 
                         id="typizerRaceStatsHeading" 
-                        class="not-sr-only text-lg leading-6 font-semibold text-gray-900 border-b p-6" 
+                        class="p-6 text-lg font-semibold leading-6 text-gray-900 border-b not-sr-only" 
                         aria-hidden="true"
                     >
                         Race Stats
@@ -126,7 +126,7 @@ import Button from './Button.vue';
                             <dt class="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">
                                 Time Elapsed
                             </dt>
-                            <dd class="order-1 text-5xl font-extrabold text-blue-500 opacity-90">
+                            <dd class="order-1 text-5xl font-extrabold opacity-90">
                                 {{ timeElapsed }}
                             </dd>
                         </div>
@@ -134,7 +134,7 @@ import Button from './Button.vue';
                             <dt class="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">
                                 WPM
                             </dt>
-                            <dd class="order-1 text-5xl font-extrabold text-blue-500 opacity-90">
+                            <dd class="order-1 text-5xl font-extrabold opacity-90">
                                 {{ wpm }}
                             </dd>
                         </div>
@@ -142,7 +142,7 @@ import Button from './Button.vue';
                             <dt class="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">
                                 Characters Typed
                             </dt>
-                            <dd class="order-1 text-5xl font-extrabold text-blue-500 opacity-90">
+                            <dd class="order-1 text-5xl font-extrabold opacity-90">
                                 {{ charactersTyped }}
                             </dd>
                         </div>
@@ -153,18 +153,18 @@ import Button from './Button.vue';
             <!-- Right 1 column -->
             <section aria-labelledby="typizerQuoteInformationHeading">
                 <div class="grid grid-cols-1 gap-4">
-                    <div class="rounded-lg bg-white overflow-hidden shadow">
+                    <div class="overflow-hidden bg-white rounded-lg shadow">
                         <!-- Quote Information -->
                         <div class="p-6 space-y-6">
                             <h2 
                                 id="typizerQuoteInformationHeading" 
-                                class="not-sr-only text-lg leading-6 font-semibold text-gray-900" 
+                                class="text-lg font-semibold leading-6 text-gray-900 not-sr-only" 
                                 aria-hidden="true"
                             >
                                 Quote Information
                             </h2>
                         </div>
-                        <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
+                        <div class="px-4 py-5 border-t border-gray-200 sm:p-0">
                             <dl class="sm:divide-y sm:divide-gray-200" aria-labelledby="typizerQuoteInformationHeading">
                                 <div class="py-4 sm:py-5 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-6">
                                     <dt class="text-sm font-semibold text-gray-500">
